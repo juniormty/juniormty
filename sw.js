f68fa18ecbb1d0db9187e83e809d07812f05dd89
Jose L Junior Garza Zuñiga <correojunior@gmail.com>
2021-04-01 16:51:23 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8fcf13c16cbd7a8e0a75.js"
+    "url": "webpack-runtime-539eee927cd8119ea253.js"
   },
   {
     "url": "framework-89ddeb149991009fa125.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "210e4aebd3184caec3cad41e106ce569"
+    "revision": "5c65b2c9b341d85f091562d3e0d767e8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-504f688893251871c6ae.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "d90ae4ed052281b30f8b2b8cde65534d"
+    "revision": "c944858202d78a817bcaaabd57c4d2ba"
   },
   {
     "url": "polyfill-b6ed67c7e16a3cb88a12.js"
